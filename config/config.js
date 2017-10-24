b9aa00df464ec7446f1926308a637ccf7a688b70
Samee Khan <samee@vt.edu>
2017-10-24 19:23:35 -0400

    adjusted config file for environment variables... attempting to connect to production db

---
+++ config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "production": {
+    "username": "beardaintweird",
+    "password": process.env.DB_PSWD,
+    "database": "groupjoin",
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "development": {
+    "username": "",
+    "password": null,
+    "database": "groupjoin",
+    "host": "localhost",
+    "dialect": "postgres"
+  }
+}
